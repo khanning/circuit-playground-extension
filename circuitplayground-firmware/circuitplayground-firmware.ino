cf809fe0e203df4bba6b454f4520fb2fc2559a9c
khanning <khanning@khanningOSX.local>
2017-02-24 12:35:37 -0500

    Update to extension and FW

--- circuitplayground-firmware/circuitplayground-firmware.ino
+++ circuitplayground-firmware/circuitplayground-firmware.ino
@@ -1,16 +1,35 @@
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_UART.h"
+#include "Adafruit_BLEGatt.h"
+
 #include <Adafruit_CircuitPlayground.h>
 
-#define UPDATE_FREQ 50000
+#define UPDATE_FREQ 200000
 #define OUTPUT_BUFFER_SIZE 10
 #define MAX_INPUT_BUFFER 16
 
-long nextUpdate;
+uint8_t SERVICE_UUID[] = {0xAA, 0x6D, 0x1E, 0xDB, 0xA1, 0xA1, 0x46, 0x40, 0x99, 0xA1, 0x9C, 0x4B, 0xF3, 0x88, 0x20, 0x5A};
+uint8_t TX_CHAR_UUID[] = {0xAA, 0x6D, 0x1E, 0xDB, 0xA1, 0xA1, 0x46, 0x41, 0x99, 0xA1, 0x9C, 0x4B, 0xF3, 0x88, 0x20, 0x5A};
+uint8_t RX_CHAR_UUID[] = {0xAA, 0x6D, 0x1E, 0xDB, 0xA1, 0xA1, 0x46, 0x42, 0x99, 0xA1, 0x9C, 0x4B, 0xF3, 0x88, 0x20, 0x5A};
 
-byte outputBuffer[OUTPUT_BUFFER_SIZE];
+Adafruit_BluefruitLE_UART ble(Serial1, -1);
+Adafruit_BLEGatt gatt(ble);
+
+uint8_t serviceId;
+uint8_t txCharId;
+uint8_t rxCharId;
+
+long nextUpdate;
+boolean newData;
+
+uint8_t outputBuffer[OUTPUT_BUFFER_SIZE];
 byte inputBuffer[MAX_INPUT_BUFFER];
 
 byte CMD_SET_LED = 0x80;
 byte CMD_CLEAR_LED = 0x81;
+byte CMD_TONE = 0x82;
+byte CMD_DIGITAL_WRITE = 0x83;
+byte CMD_ANALOG_WRITE = 0x84;
 
 boolean waitForCommand = false;
 byte commandSize = 0;
@@ -19,50 +38,37 @@ byte commandIndex = 0;
 void setup() {
   CircuitPlayground.begin();
   CircuitPlayground.setAccelRange(LIS3DH_RANGE_2_G);
-  Serial.begin(9600);
-  Serial1.begin(9600);
+  //Serial.begin(9600);
+  initBLE();
+  //Serial.println("starting serial");
   nextUpdate = micros() + UPDATE_FREQ;
 }
 
 void loop() {
+  uint8_t tmpBuffer[MAX_INPUT_BUFFER];
+  byte i;
   
-  if (Serial1.available() > 0) {
-    if (!waitForCommand) {
-      inputBuffer[0] = Serial1.read();
-      if (inputBuffer[0] == CMD_SET_LED) {
-        waitForCommand = true;
-        commandSize = 4;
-        commandIndex = 1;
-      } else if (inputBuffer[0] == CMD_CLEAR_LED) {
-        waitForCommand = true;
-        commandSize = 1;
-        commandIndex = 1;
-      }
-    } else {
-      inputBuffer[commandIndex++] = Serial1.read();
-      if (commandIndex >= commandSize) {
-        processCmd();
-        waitForCommand = false;
-      }
+  updateSensors();
+  gatt.setChar(txCharId, outputBuffer, OUTPUT_BUFFER_SIZE);
+  gatt.getChar(rxCharId, tmpBuffer, MAX_INPUT_BUFFER);
+  for (i=0; i<MAX_INPUT_BUFFER; i++) {
+    if (tmpBuffer[i] != inputBuffer[i]) {
+      memcpy(inputBuffer, tmpBuffer, sizeof(tmpBuffer[0])*MAX_INPUT_BUFFER);
+      processCmd(inputBuffer);
+      break;
     }
   }
   
-  updateSensors();
-  if ((long) (micros() - nextUpdate) >= 0) {
-    Serial1.write(outputBuffer, OUTPUT_BUFFER_SIZE);
-    Serial1.write('\n');
-    nextUpdate += UPDATE_FREQ;
-  }
-  delay(1);
 }
 
-void processCmd() {
+void processCmd(uint8_t inputBuffer[]) {
   if (inputBuffer[0] == CMD_SET_LED) {
     if (inputBuffer[1] == 0) {
       for (byte i=0; i<10; i++) {
         CircuitPlayground.setPixelColor(i, inputBuffer[2], inputBuffer[3], inputBuffer[4]);  
       }
     } else {
+      Serial.println(inputBuffer[4]);
       CircuitPlayground.setPixelColor(inputBuffer[1]-1, inputBuffer[2], inputBuffer[3], inputBuffer[4]);  
     }
   } else if (inputBuffer[0] == CMD_CLEAR_LED) {
@@ -71,11 +77,40 @@ void processCmd() {
     } else {
       CircuitPlayground.setPixelColor(inputBuffer[1]-1, 0, 0, 0);  
     }
+  } else if (inputBuffer[0] == CMD_TONE) {
+    int t = inputBuffer[1] << 8 | inputBuffer[2];
+    CircuitPlayground.playTone(t, inputBuffer[3]*1000);
+  } else if (inputBuffer[0] == CMD_DIGITAL_WRITE) {
+    digitalWrite(inputBuffer[1], inputBuffer[2]);
+  } else if (inputBuffer[0] == CMD_ANALOG_WRITE) {
+    digitalWrite(inputBuffer[1], map(inputBuffer[2], 0, 100, 0, 1023));
   }
+}
+
+void initBLE() {
+  ble.begin(false);
+  ble.factoryReset();
+  ble.echo(false);
+  ble.setInterCharWriteDelay(5);
+
+  ble.sendCommandCheckOK(F("AT+GAPDEVNAME=CircuitPlayground Scratch"));
+
+  serviceId = gatt.addService(SERVICE_UUID);
+  txCharId = gatt.addCharacteristic(TX_CHAR_UUID, GATT_CHARS_PROPERTIES_READ | GATT_CHARS_PROPERTIES_NOTIFY, 10, 10, BLE_DATATYPE_BYTEARRAY);
+  rxCharId = gatt.addCharacteristic(RX_CHAR_UUID, GATT_CHARS_PROPERTIES_READ | GATT_CHARS_PROPERTIES_WRITE, 16, 16, BLE_DATATYPE_BYTEARRAY);
+  
+  uint8_t advdata[] = {0x02, 0x01, 0x06, 0x11, 0x06, 0x5A, 0x20, 0x88, 0xF3, 0x4B, 0x9C, 0xA1, 0x99, 0x40, 0x46, 0xA1, 0xA1, 0xDB, 0x1E, 0x6D, 0xAA};
+  ble.setAdvData(advdata, sizeof(advdata));
+  
+  ble.setInterCharWriteDelay(4);
+  ble.reset();
+
+  //ble.setBleGattRxCallback(rxCharId, onRxWrite);
 }
 
 void updateSensors() {
   int temp, light;
+  byte i;
   
   outputBuffer[0] = CircuitPlayground.leftButton();
   outputBuffer[1] = CircuitPlayground.rightButton();
@@ -92,4 +127,11 @@ void updateSensors() {
   outputBuffer[7] = CircuitPlayground.motionX();
   outputBuffer[8] = CircuitPlayground.motionY();
   outputBuffer[9] = CircuitPlayground.motionZ();
+
+  /*for (i=0; i<OUTPUT_BUFFER_SIZE; i++) {
+    if (tmpBuffer[i] != outputBuffer[i]) {
+      outputBuffer[i] = tmpBuffer[i];
+      newData = true;
+    }
+  }*/
 }
--- circuitplayground_extension.js
+++ circuitplayground_extension.js
@@ -14,11 +14,14 @@
 
 (function(ext) {
 
-  var UUID = '6e400001b5a3f393e0a9e50e24dcca9e',
-    TX_CHAR = '6e400002b5a3f393e0a9e50e24dcca9e',
-    RX_CHAR = '6e400003b5a3f393e0a9e50e24dcca9e';
+  var UUID = 'aa6d1edba1a1464099a19c4bf388205a',
+    RX_CHAR = 'aa6d1edba1a1464199a19c4bf388205a',
+    TX_CHAR = 'aa6d1edba1a1464299a19c4bf388205a';
 
   var BUFFER_SIZE = 10;
+
+  var CMD_DIGITAL_WRITE = 0x83,
+      CMD_ANALOG_WRITE = 0x84;
 
   var rawData = [];
   var inputData = null;
@@ -37,6 +40,34 @@
     if (val > aMax) val = aMax;
     else if (val < aMin) val = aMin;
     return (((bMax - bMin) * (val - aMin)) / (aMax - aMin)) + bMin;
+  }
+
+  function getTiltAngle(dir) {
+    if (dir === 'up') {
+      if (inputData[7] > 10) {
+        return (256 - inputData[7]) * 10;
+      } else {
+        return inputData[7] * -10;
+      }
+    } else if (dir === 'down') {
+      if (inputData[7] > 10) {
+        return (256 - inputData[7]) * -10;
+      } else {
+        return inputData[7] * 10;
+      }
+    } else if (dir === 'left') {
+      if (inputData[8] > 10) {
+        return (256 - inputData[8]) * 10;
+      } else {
+        return inputData[8] * -10;
+      }
+    } else if (dir === 'right') {
+      if (inputData[8] > 10) {
+        return (256 - inputData[8]) * -10;
+      } else {
+        return inputData[8] * 10;
+      }
+    }
   }
 
   ext.whenButtonPressed = function(btn) {
@@ -70,7 +101,17 @@
     var g = (c >> 8) & 0xFF;
     var b = c & 0xFF;
     var output = [0x80, led, r, g, b];
-    console.log(output);
+    device.emit('write', {uuid: TX_CHAR, bytes: output});
+  };
+
+  ext.setLEDRGB = function(l, r, g, b) {
+    var led = parseInt(l);
+    if (l === 'all')
+      led = 0;
+    if (!Number.isInteger(r)) return;
+    if (!Number.isInteger(g)) return;
+    if (!Number.isInteger(b)) return;
+    var output = [0x80, led, r, g, b];
     device.emit('write', {uuid: TX_CHAR, bytes: output});
   };
 
@@ -86,8 +127,20 @@
     return inputData[2];
   };
 
-  ext.getTemp = function() {
-    return (inputData[3] << 8 | inputData[4]);
+  ext.getTemp = function(type) {
+    var reading = (inputData[3] << 8) | inputData[4];
+    reading = ((1023 * 10000) / reading);
+    reading -= 10000;
+    var steinhart = reading / 10000;
+    steinhart = Math.log(steinhart);
+    steinhart /= 3380;
+    steinhart += 1.0 / (25 + 273.15);
+    steinhart = 1.0 / steinhart;
+    steinhart -= 273.15;
+    if (type === "F")
+      return steinhart * 1.8 + 32;
+    else
+      return steinhart;
   };
 
   ext.getBrightness = function() {
@@ -95,60 +148,50 @@
     return Math.round(map(brightness, 0, 1023, 0, 100));
   };
 
+  ext.playNote = function(note, dur) {
+    var freq = 440 * Math.pow(1.05946309436, note-69);
+    freq = Math.round(freq);
+    if (dur < 0) return;
+    if (dur > 255) dur = 255;
+    var output = [0x82, freq >> 8, freq & 0xFF, dur];
+    console.log(output);
+    device.emit('write', {uuid: TX_CHAR, bytes: output});
+  };
+
   ext.whenTilted = function(dir) {
     if (dir === 'any')
       return (inputData[7] != 0 || inputData[8] != 0);
-    else if (dif === 'up')
-      return (256 - inputData[7]) > 0;
-    else if (dif === 'down')
-      return inputData[7] > 0;
-    else if (dif === 'left')
-      return (256 - inputData[8]) > 0;
-    else if (dif === 'right')
-      return inputData[8] > 0;
-    return false;
-  };
+    else
+      return getTiltAngle(dir) >= 10;
+  }
 
   ext.isTilted = function(dir) {
     if (dir === 'any')
       return (inputData[7] != 0 || inputData[8] != 0);
-    else if (dif === 'up')
-      return (256 - inputData[7]) > 0;
-    else if (dif === 'down')
-      return inputData[7] > 0;
-    else if (dif === 'left')
-      return (256 - inputData[8]) > 0;
-    else if (dif === 'right')
-      return inputData[8] > 0;
-    return false;
+    else
+      return getTiltAngle(dir) >= 10;
   };
 
   ext.getTiltAngle = function(dir) {
-    if (dir === 'up') {
-      if (inputData[7] > 10) {
-        return (256 - inputData[7]) * 10;
-      } else {
-        return inputData[7] * -10;
-      }
-    } else if (dir === 'down') {
-      if (inputData[7] > 10) {
-        return (256 - inputData[7]) * -10;
-      } else {
-        return inputData[7] * 10;
-      }
-    } else if (dir === 'left') {
-      if (inputData[8] > 10) {
-        return (256 - inputData[8]) * 10;
-      } else {
-        return inputData[8] * -10;
-      }
-    } else if (dir === 'right') {
-      if (inputData[8] > 10) {
-        return (256 - inputData[8]) * -10;
-      } else {
-        return inputData[8] * 10;
-      }
-    }
+    return getTiltAngle(dir);
+  };
+
+  ext.setClipDigital = function(pin, state) {
+    if (pin != 6 && pin != 9 && pin != 10 && pin != 11)
+      return;
+    var output = [CMD_DIGITAL_WRITE, pin, 0];
+    if (state === "on")
+      output[2] = 1;
+    device.emit('write', {uuid: TX_CHAR, bytes: output});
+  };
+
+  ext.setClipAnalog = function(pin, val) {
+    if (pin != 6 && pin != 9 && pin != 10 && pin != 11)
+      return;
+    if (val > 100) val = 100;
+    else if (val < 0) val = 0;
+    var output = [CMD_ANALOG_WRITE, pin, val];
+    device.emit('write', {uuid: TX_CHAR, bytes: output});
   };
 
   ext._getStatus = function() {
@@ -169,18 +212,9 @@
     device.open(function(d) {
       if (device == d) {
         device.on(RX_CHAR, function(bytes) {
-          for (var i=0; i<bytes.data.length; i++) {
-            if (bytes.data[i] != 0xA) {
-              rawData = rawData.concat(bytes.data[i]);
-            } else {
-              if (rawData.length == BUFFER_SIZE) {
-                //processInput(rawData);
-                inputData = rawData;
-              } else {
-                console.log("wrong data size");
-              }
-              rawData = [];
-            }
+          if (bytes.data.length === 10) {
+            inputData = bytes.data;
+            console.log(inputData);
           }
         });
       } else if (d) {
@@ -205,27 +239,34 @@
 
   var blocks = [
     ['h', 'when %m.btnSides button pressed', 'whenbuttonPressed', 'left'],
-    [' '],
     ['b', '%m.btnSides button pressed?', 'isButtonPressed', 'left'],
+    [' '],
     ['b', 'slider', 'getSlider'],
-    [' '],
-    [' ', 'set LED %d.leds to %c', 'setLED', '1', 0xFF0000],
-    [' ', 'turn LED %d.leds off', 'clearLED', '1'],
-    [' '],
-    ['r', 'temperature', 'getTemp'],
-    [' '],
+    ['r', 'temperature in %m.temp', 'getTemp', 'F'],
     ['r', 'brightness', 'getBrightness'],
     [' '],
-    ['h', 'when tilted %m.tiltDirs', 'whenTilted', 'any'],
+    [' ', 'set LED %d.leds to %c', 'setLED', '1', 0xFF0000],
+    [' ', 'set LED %d.leds to R:%n G:%n B:%n', 'setLEDRGB', '1', 0, 255, 0],
+    [' ', 'turn LED %d.leds off', 'clearLED', '1'],
+    [' '],
+    [' ', 'play note %d.note for %n second', 'playNote', 60, 1],
+    [' '],
+    ['h', 'when tilted %m.tiltDirs', 'isTilted', 'any'],
     ['b', 'tilted %m.tiltDirs ?', 'isTilted', 'any'],
-    ['r', 'tilt angle %m.tiltAngleDirs', 'getTiltAngle', 'up']
+    ['r', 'tilt angle %m.tiltAngleDirs', 'getTiltAngle', 'up'],
+    [' '],
+    [' ', 'set pin %d.clipPins %m.states', 'setClipDigital', 6, 'on'],
+    [' ', 'set pin %d.clipPins to %n%', 'setClipAnalog', 6, '50']
   ];
 
   var menus = {
     btnSides: ['left', 'right', 'any'],
     leds: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'all'],
     tiltDirs: ['any', 'up', 'down', 'left', 'right'],
-    tiltAngleDirs: ['up', 'down', 'left', 'right']
+    tiltAngleDirs: ['up', 'down', 'left', 'right'],
+    temp: ['F', 'C'],
+    clipPins: [6, 9, 10, 12],
+    states: ['on', 'off']
   };
 
   var descriptor = {
